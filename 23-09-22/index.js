ae0374cb756abf9ec5140c84b38b9bda707aa757
Sstark97 <aitorscinfo@gmail.com>
2022-09-24 20:03:16 +0100

    Solve all exercises

--- 23-09-22/index.js
+++ 23-09-22/index.js
@@ -94,8 +94,86 @@ const sumRange = (values, ranges) => {
         }
     }
 
-    return allSum.sort((a,b) => a + b)[0]
+    return `${JSON.stringify(values)} --- ${JSON.stringify(ranges)} --- resultado: ${allSum.sort((a,b) => a + b)[0]}`
 
 }
 
-console.log(sumRange([1, -2, 3, 4, -5, -4, 3, 2, 1 ],[[1, 3, 10]]))
+// console.log(sumRange([1, -2, 3, 4, -5, -4, 3, 2, 1 ],[[1, 3, 10]]))
+
+/*
+    Dado un mensaje de entrada (solicitado al usuario), vamos a encriptarlo
+    siguiendo las siguientes reglas:
+        1. Mover las consonantes 9 lugares hacia adelante a través del alfabeto. Si
+        pasan de la ‘z’, se vuelve a comenzar en ‘a’
+        2. Las vocales se mueven 5 lugares hacia atrás a través del alfabeto
+        Excepciones:
+            Si el carácter es 'c' o 'o', muévalo 1 lugar hacia atrás.
+            Para 'd' muévelo hacia atrás 3 lugares.
+            Para 'e', muévelo hacia atrás 4.
+        3. Si una letra movida se convierte en 'c', 'o', 'd' o 'e', revertirla a su valor
+        original.
+    Finalmente tener en cuenta que la cadena que proporcionamos para encriptar
+    ha de estar en minúsculas, no debe tener caracteres especiales y no puede ser
+    vacía.
+*/
+
+const alphabet = "abcdefghijklmnñopqrstuvwxyz"
+
+const comprobePhrase = (phrase) => !/^[a-z]+$/.test(phrase)
+
+const comprobeEspecialCase = (letter, letterPos) => {
+
+    const especialCase = {
+        c: 1,
+        o: 1,
+        d: 3,
+        e: 4
+    }
+
+    return  especialCase[letter] ? letterPos - especialCase[letter] : -1
+}
+
+const consonant = (letterPos, alphabetLetter) => {
+    const thirdCase = "code"
+    let letter = ""
+
+    if(letterPos + 9 > alphabet.length - 1) {
+        const newPos = 9 - (alphabet.length - letterPos)
+        letter += !thirdCase.includes(alphabet[newPos]) ? alphabet[newPos] : alphabetLetter
+    } else {
+        letter += !thirdCase.includes(alphabet[letterPos]) ? alphabet[letterPos + 9] : alphabetLetter
+    } 
+
+    return letter
+}
+
+const encryptPhrase = () => {
+    const vowels = "aeiou"
+    const phrase = prompt("Introduce la frase: ")
+    let encrypt = ""
+
+    if(comprobePhrase(phrase)) {
+        alert("La cadena no puede contener mayúsculas, caracteres especiales y no puede ser vacío")
+        return
+    }
+
+    for(let letter = 0; letter < phrase.length; letter++) {
+        const letterPos = alphabet.indexOf(phrase[letter])
+        const alphabetLetter = alphabet[letterPos]
+        const especial = comprobeEspecialCase(alphabetLetter,letterPos)
+
+        if (parseInt(especial) !== -1){
+            encrypt += alphabet[especial]
+        } else if(!vowels.includes(alphabetLetter)) {
+            encrypt += consonant(letterPos, alphabetLetter)
+        } else {
+            const newPos = letterPos - 5 < 0 ? (alphabet.length - letterPos) - 5 : letterPos
+            encrypt += alphabet[newPos]
+        }
+
+    }
+
+    alert(`El código cifrado es: ${encrypt}`)
+}
+
+encryptPhrase()
