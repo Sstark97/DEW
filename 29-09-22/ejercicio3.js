8c4e49efcaf6942c373fcf7572eb2d15d7faf343
Sstark97 <aitorscinfo@gmail.com>
2022-09-30 12:32:32 +0100

    Final 29-09-22 third exercise

--- 29-09-22/ejercicio3.js
+++ 29-09-22/ejercicio3.js
@@ -4,10 +4,20 @@
 */
 
 const getDateInFormat = format => {
-    const options = { year: 'numeric', month: '2-digit', day: 'numeric' };
+    const language = format === 2 ? "es-ES" : "en-US"
+
+    const commonOptions = {
+        year: 'numeric', month: 'long', day: 'numeric', weekday: "long" 
+    }
+    const allOptions = {
+        1: { year: 'numeric', month: '2-digit', day: 'numeric' },
+        2: {...commonOptions},
+        3: {...commonOptions}
+    }
+
     const today  = new Date();
     
-    console.log(today.toLocaleDateString("es-ES",options)); // 9/17/2016
+    console.log(today.toLocaleDateString(language,allOptions[format]))
 }
 
-getDateInFormat()
+getDateInFormat(3)
