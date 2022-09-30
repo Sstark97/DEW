100fb09a93c5e9eb740cee65e4ac2549a45baf39
aitor <aitorscinfo@gmail.com>
2022-09-30 15:44:12 +0100

    Start exercise 5

---
+++ 29-09-22/ejercicio5.js
@@ -0,0 +1,32 @@
+/*
+    Crea un programa que pida al usuario que elija una opción del siguiente menú:
+    • 1) Potencia.
+    • 2) Raíz.
+    • 3) Redondeo.
+    • 4) Trigonometría.
+    • Si el usuario introduce 1, se le deberá pedir una base y un exponente y se mostrará el
+    resultado en pantalla (La potencia de X elevado a Y es: )
+    • Si el usuario introduce 2, se le pedirá un número (no negativo) y se mostrará el
+    resultado en pantalla (La raíz de X es: )
+    • Si el usuario introduce 3, se le pedirá un decimal por pantalla y se mostrará el redondeo
+    al entero más próximo, al alta y a la baja.
+    • Si el usuario introduce 4, se le pedirá un ángulo (entre 0 y 360) y se le mostrarán por
+    pantalla los valores trigonométricos del seno, coseno y tangente.
+*/
+
+const pow = () => {
+    const base = parseInt(prompt("Diga la base"))
+}
+
+const options = {
+
+}
+
+const mathCalculator = () => {
+    let stop = false;
+
+    while(!stop) {
+        const option = parseInt(prompt("Seleccione una opción de Calculo: \n1) Potencia\n2)Raíz\n3)Redondeo\n4)Trigonometría\n5)Salir"));
+
+    }
+}
