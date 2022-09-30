6d4e34071ca0818e7207db5b4cc1a5c0902eb77f
Sstark97 <aitorscinfo@gmail.com>
2022-09-30 12:42:18 +0100

    Final 29-09-22 third exercise

--- 29-09-22/ejercicio3.js
+++ 29-09-22/ejercicio3.js
@@ -3,21 +3,39 @@
     el usuario por parámetro:
 */
 
-const getDateInFormat = format => {
-    const language = format === 2 ? "es-ES" : "en-US"
-
-    const commonOptions = {
-        year: 'numeric', month: 'long', day: 'numeric', weekday: "long" 
-    }
-    const allOptions = {
-        1: { year: 'numeric', month: '2-digit', day: 'numeric' },
-        2: {...commonOptions},
-        3: {...commonOptions}
-    }
-
-    const today  = new Date();
-    
-    console.log(today.toLocaleDateString(language,allOptions[format]))
+const commonOptions = {
+    year: 'numeric', month: 'long', day: 'numeric', weekday: "long" 
+}
+const allOptions = {
+    1: { year: 'numeric', month: '2-digit', day: 'numeric' },
+    2: {...commonOptions},
+    3: {...commonOptions}
 }
 
-getDateInFormat(3)
+const comprobeFormat = format => {
+    if(![1,2,3].includes(format)) {
+        return `${format} no es un formato disponible, escriba un formato válido:
+        1: 17/02/2016
+        2: Miércoles, 17 de febrero de 2016
+        3: Wednesday, February 17, 2016
+        `
+    }
+
+    return ""
+}
+
+const getDateInFormat = format => {
+    const formatComprobe = comprobeFormat(format)
+
+    if(formatComprobe !== "") {
+        return formatComprobe
+    }
+
+    const language = format === 2 ? "es-ES" : "en-US"
+
+    const today  = new Date()
+    
+    return today.toLocaleDateString(language,allOptions[format])
+}
+
+console.log(getDateInFormat(1))
