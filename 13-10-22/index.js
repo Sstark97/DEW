6cba28589d02bf937d196dc09379fa5b235180b0
Sstark97 <aitorscinfo@gmail.com>
2022-10-15 11:41:38 +0100

    Final 13-10-22 exercise

--- 13-10-22/index.js
+++ 13-10-22/index.js
@@ -23,11 +23,11 @@ const getInputValue = () => {
 const getAlertMsg = () => {
     let message = ""
 
-    message += rows.classList.value === "error" ? "Errores:\nLas filas deben ser números del 1-12\n" : ""
-    message += cols.classList.value === "error" ? "Las columnas deben ser números del 1-12\n" : ""
-    message += color.classList.value === "error" ? "El color es inválido\n" : ""
-    message += font.classList.value === "error" ? "La fuente es inválida\n" : ""
-    message += fontSize.classList.value === "error" ? "El tamaño de la fuente debe ser un número del 10-14\n" : ""
+    message += rows.classList.value.includes("bg-danger") ? "Errores:\nLas filas deben ser números del 1-12\n" : ""
+    message += cols.classList.value.includes("bg-danger") ? "Las columnas deben ser números del 1-12\n" : ""
+    message += color.classList.value.includes("bg-danger") ? "El color es inválido\n" : ""
+    message += font.classList.value.includes("bg-danger") ? "La fuente es inválida\n" : ""
+    message += fontSize.classList.value.includes("bg-danger") ? "El tamaño de la fuente debe ser un número del 10-14\n" : ""
 
     return message
 }
@@ -35,45 +35,64 @@ const getAlertMsg = () => {
 const comprobeErrors = () => {
     const [ rowsValue, colsValue, colorValue, fontValue, fontSizeValue ] = getInputValue()
 
-    rows.classList = typeof rowsValue !== "number" || !maxRowsCols.includes(rowsValue) ? "error" : ""
-    cols.classList = typeof colsValue !== "number" || !maxRowsCols.includes(colsValue) ? cols.classList = "error" : ""
-    color.classList = !colors.includes(colorValue) ? "error" : ""
-    font.classList = !fonts.includes(fontValue) ? "error" : ""
-    fontSize.classList = typeof fontSizeValue !== "number" || !maxFonts.includes(fontSizeValue) ? "error" : ""
-    
-    const message = getAlertMsg()
+    rows.classList = typeof rowsValue !== "number" || !maxRowsCols.includes(rowsValue) ? "form-control bg-danger" : "form-control"
+    cols.classList = typeof colsValue !== "number" || !maxRowsCols.includes(colsValue) ? "form-control bg-danger" : "form-control"
+    color.classList = !colors.includes(colorValue) ? " form-control bg-danger" : "form-control"
+    font.classList = !fonts.includes(fontValue) ? "form-control bg-danger" : "form-control"
+    fontSize.classList = typeof fontSizeValue !== "number" || !maxFonts.includes(fontSizeValue) ? "form-control bg-danger" : "form-control"
 
-    return message
+    return getAlertMsg()
+}
+
+const removePrevHtml = () => {
+    generate.innerHTML = ""
+    const beforeH2 = document.querySelector("h2")
+    if(beforeH2 !== null) beforeH2.remove()
 }
 
 const generateTable = e => {
     e.preventDefault()
+
+    // Control de Errores
     const message = comprobeErrors()
     
     if(message !== "") {
+        console.log(message)
         alert(message)
         return
     }
 
+    // Traigo los valores de cada input y elimino el HTML Previo
     const [ rowsValue, colsValue, colorValue, fontValue, fontSizeValue ] = getInputValue()
-    generate.innerHTML = ""
+    removePrevHtml()
 
+    // Genero la Tabla
     const trs = []
 
     for(let row = 0; row < rowsValue; row ++) {
         const tr = document.createElement("tr")
         tr.style.background = row % 2 !== 0 && colorValue
-        let tds = []
+        const tds = []
 
         for(let col = 0; col < colsValue; col ++) {
             const td = document.createElement("td")
             td.textContent = `F${row}, C${col}`
+            td.style.color = row % 2 !== 0 && "#fff"
             tds.push(td)
         }
         tr.append(...tds)
         trs.push(tr)
     }
 
+    // Creo un H2 con el tamaño de la tabla
+    const h2 = document.createElement("h2")
+    h2.textContent = `Tabla de ${rowsValue} x ${colsValue}`
+    h2.classList = "text-center"
+
+    // Inserto el H2 antes de la Tabla
+    generate.insertAdjacentElement("beforebegin",h2);
+
+    // Agrego los trs a la tabla y le añado la fuente y su tamaño
     generate.append(...trs)
     generate.style.fontSize = `${fontSizeValue}px`
     generate.style.fontFamily = fontValue
