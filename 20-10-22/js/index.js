0419b18df9b741c37e34be79feca5409e1d62b0d
aitor <aitorscinfo@gmail.com>
2022-10-20 19:57:22 +0100

    Add Calculator class

--- 20-10-22/js/index.js
+++ 20-10-22/js/index.js
@@ -1,3 +1,16 @@
 import Calculator from "./Calculator.js";
 
 const calculator = new Calculator()
+
+const calculatorDOM = document.querySelector("main div")
+const output = document.querySelector("output")
+
+calculatorDOM.addEventListener("click", e => {
+    const element = e.target
+
+    if(element.nodeName === "BUTTON" && /[0-9]$/.test(element.textContent) || element.textContent === ",") {
+        if(output.textContent.length < 12) {
+            output.textContent = output.textContent != 0 ? output.value + element.textContent : element.textContent
+        }
+    }
+})
