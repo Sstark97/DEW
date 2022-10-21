3e7c33705fe1db4c4f3c4d40f16bfcf8c6a3a114
Sstark97 <aitorscinfo@gmail.com>
2022-10-21 10:43:45 +0100

    Add Calculator State

--- 20-10-22/js/index.js
+++ 20-10-22/js/index.js
@@ -1,6 +1,11 @@
 import Calculator from "./Calculator.js";
 
 const calculator = new Calculator()
+const calculatorState = {
+    firstNumber: undefined,
+    operator: undefined,
+    secondNumber: undefined
+}
 
 const calculatorDOM = document.querySelector("main div")
 const output = document.querySelector("output")
@@ -8,9 +13,11 @@ const output = document.querySelector("output")
 calculatorDOM.addEventListener("click", e => {
     const element = e.target
 
-    if(element.nodeName === "BUTTON" && /[0-9]$/.test(element.textContent) || element.textContent === ",") {
-        if(output.textContent.length < 12) {
-            output.textContent = output.textContent != 0 ? output.value + element.textContent : element.textContent
+    if(element.nodeName === "BUTTON" && output.textContent.length < 12) {
+        const [firstNumber, operator, secondNumber] = calculatorState
+
+        if((/[0-9]$/.test(element.textContent) || element.textContent === ",") ) {
+            firstNumber += element.textContent
         }
     }
 })
