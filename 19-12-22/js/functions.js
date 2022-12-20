76f00a9146c63d868757c05def95038d9a1d607c
aitor <aitorscinfo@gmail.com>
2022-12-20 18:41:55 +0000

    Finish 19-12-22

--- 19-12-22/js/functions.js
+++ 19-12-22/js/functions.js
@@ -12,24 +12,24 @@ const cookieToArray = () => {
 }
 
 const createHeader = () => {
-    const headerExist = document.querySelector("#header_task") 
+    const headerExist = document.querySelector('#header_task') 
 
     if(headerExist) {
         headerExist.delete()
         return
     }
 
-    const header = document.createElement("div")
-    const element = document.createElement("div")
-    const actions = document.createElement("div")
+    const header = document.createElement('div')
+    const element = document.createElement('div')
+    const actions = document.createElement('div')
 
-    element.textContent = "Productos"
-    element.className = "w-3/4 pl-6 text-lg font-bold"
+    element.textContent = 'Productos'
+    element.className = 'w-3/4 pl-6 text-lg font-bold'
 
-    actions.textContent = "Acciones"
-    actions.className = "w-1/4 pr-6 text-lg font-bold"
+    actions.textContent = 'Acciones'
+    actions.className = 'w-1/4 pr-6 text-lg font-bold'
 
-    header.className = "mt-5 w-full flex justify-center items-center border-b-2 border-slate-200 pb-3"
+    header.className = 'mt-5 w-full flex justify-center items-center border-b-2 border-slate-200 pb-3'
 
     header.append(element, actions)
 
@@ -38,33 +38,33 @@ const createHeader = () => {
 
 const createTable = (values, container) => {
     if(values.length === 0) {
-        container.textContent = "No hay elementos en el carrito"
+        container.textContent = 'No hay elementos en el carrito'
         return
     }
 
-    const elements = [];
-    container.innerHTML = "";
-    container.className += values.length !== 0 ? "border border-slate-200 rounded" : null;
-    container.append(createHeader());
+    const elements = []
+    container.innerHTML = ''
+    container.className += values.length !== 0 ? 'border border-slate-200 rounded' : null
+    container.append(createHeader())
 
     values.forEach( (value, index) => {
-        const div = document.createElement("div");
-        const p = document.createElement("p");
-        const btn = document.createElement("button");
-        const icon = document.createElement("i");
+        const div = document.createElement('div')
+        const p = document.createElement('p')
+        const btn = document.createElement('button')
+        const icon = document.createElement('i')
 
-        div.className = "mt-5 w-full flex justify-center items-center border-b-2 border-slate-200 pb-3";
-        p.className = "w-3/4 pl-6";
-        btn.className = "w-1/4 pr-6";
-        icon.className = "bx bx-trash bg-red-400 p-2 rounded delete"
+        div.className = 'mt-5 w-full flex justify-center items-center border-b-2 border-slate-200 pb-3'
+        p.className = 'w-3/4 pl-6'
+        btn.className = 'w-1/4 pr-6'
+        icon.className = 'bx bx-trash bg-red-400 p-2 rounded delete'
         
         icon.id = index
-        p.textContent = value;
+        p.textContent = value
 
-        btn.append(icon);
-        div.append(p, btn);
+        btn.append(icon)
+        div.append(p, btn)
     
-        elements.push(div);
+        elements.push(div)
     })
 
     container.append(...elements)
--- 19-12-22/js/index.js
+++ 19-12-22/js/index.js
@@ -1,35 +1,35 @@
-import { cookieToArray, createTable } from "./functions.js";
+import { cookieToArray, createTable } from './functions.js'
 
-const main = document.querySelector("main");
-const input = document.querySelector("input");
-const container =document.querySelector("#container");
+const main = document.querySelector('main')
+const input = document.querySelector('input')
+const container =document.querySelector('#container')
 
-let values = cookieToArray() ?? [];
+let values = cookieToArray() ?? []
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
     if(values.length !== 0) {
         createTable(values, container)
     }
 })
 
-main.addEventListener("click", e => {
-    const element = e.target;
+main.addEventListener('click', e => {
+    const element = e.target
 
-    if (element.className.includes("add")){
-        if (input.value !== ""){
-            values.push(input.value);
-            document.cookie = `values=[${values}]`;
+    if (element.className.includes('add')){
+        if (input.value !== ''){
+            values.push(input.value)
+            document.cookie = `values=[${values}]`
 
-            input.value = ""
+            input.value = ''
             createTable(values, container)
         }
         
-    } else if (element.className.includes("delete")) {
+    } else if (element.className.includes('delete')) {
         const pos = parseInt(element.id)
 
         values = values.filter((e, index) => index !== pos)
-        document.cookie = values.length !== 0 ? `values=[${values}]` : 'values= ;max-age=0';
-        container.className = values.length !== 0 ? container.className : container.className.replace("border border-slate-200", "");
+        document.cookie = values.length !== 0 ? `values=[${values}]` : 'values= max-age=0'
+        container.className = values.length !== 0 ? container.className : container.className.replace('border border-slate-200', '')
 
         createTable(values, container)
     }
